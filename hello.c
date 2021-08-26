f7d975d1e12a7afe259ea3743d097ab5b7abd4f1
Matthew <Mgeary2@huskers.unl.edu>
2021-08-26 18:56:35 +0000

    added my major to output

--- hello.c
+++ hello.c
@@ -1,10 +1,17 @@
-#include <stdio.h>
-#include <stdlib.h>
+/**
+ * Author: Matthew Geary Jackson Ye Jung Cho
+ * Date: 2021/08/14
+ *
+ * A simple hello world program in C
+ *
+ */
+ #include <stdio.h>
+ #include <stdlib.h>
 
 int main (int x, char **y){
 
 	printf("Hello World, I'm Ye Jung Cho\n");
-	printf("My major is Civil Engineering!\n");
+	printf("My major is Civil Engineering!\nMatthew Geary/nComputer Science");
 
 	return 0;
 
